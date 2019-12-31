c691ecfa3276d5a82cd2d75a2e52fa9e0246c4ac
CNCYY <961947088@qq.com>
2019-12-31 14:40:42 +0800

    新建第二题文件夹

---
+++ animate/animate.js
@@ -0,0 +1,593 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"animate_atlas_", frames: [[0,0,1330,1238]]},
+		{name:"animate_atlas_2", frames: [[0,0,1278,1278]]},
+		{name:"animate_atlas_3", frames: [[0,0,1218,1290]]},
+		{name:"animate_atlas_4", frames: [[0,1090,1078,642],[0,0,1078,1088]]},
+		{name:"animate_atlas_5", frames: [[1255,757,126,159],[0,822,817,132],[978,0,976,296],[1255,623,489,132],[0,596,1253,224],[1670,298,326,132],[0,0,976,368],[1874,723,126,159],[1746,432,249,128],[1255,298,413,189],[1746,723,126,159],[1907,562,126,159],[1746,562,159,159],[1255,489,489,132],[0,370,1253,224]]}
+];
+
+
+// symbols:
+
+
+
+(lib.CachedTexturedBitmap_1 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_10 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_11 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_12 = function() {
+	this.initialize(ss["animate_atlas_"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_13 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(3);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_14 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(4);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_15 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(5);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_16 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(6);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_18 = function() {
+	this.initialize(ss["animate_atlas_4"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_19 = function() {
+	this.initialize(ss["animate_atlas_2"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_2 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(7);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_20 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(8);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_21 = function() {
+	this.initialize(ss["animate_atlas_4"]);
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_25 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(9);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_3 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(10);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_4 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(11);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_5 = function() {
+	this.initialize(ss["animate_atlas_3"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_6 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(12);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_8 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(13);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_9 = function() {
+	this.initialize(ss["animate_atlas_5"]);
+	this.gotoAndStop(14);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.button4 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// 图层_1
+	this.instance = new lib.CachedTexturedBitmap_25();
+	this.instance.parent = this;
+	this.instance.setTransform(-6.55,-6.55,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-6.5,-6.5,206.5,94.5);
+
+
+(lib.button3 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// 图层_1
+	this.instance = new lib.CachedTexturedBitmap_25();
+	this.instance.parent = this;
+	this.instance.setTransform(-6.55,-6.55,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-6.5,-6.5,206.5,94.5);
+
+
+(lib.button2 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// 图层_1
+	this.instance = new lib.CachedTexturedBitmap_25();
+	this.instance.parent = this;
+	this.instance.setTransform(-6.55,-6.55,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-6.5,-6.5,206.5,94.5);
+
+
+(lib.button1 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// 图层_1
+	this.instance = new lib.CachedTexturedBitmap_25();
+	this.instance.parent = this;
+	this.instance.setTransform(-6.55,-6.55,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-6.5,-6.5,206.5,94.5);
+
+
+(lib.场景_1_图层_1 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// 图层_1
+	this.instance = new lib.CachedTexturedBitmap_6();
+	this.instance.parent = this;
+	this.instance.setTransform(617.05,312.65,0.5,0.5);
+
+	this.instance_1 = new lib.CachedTexturedBitmap_5();
+	this.instance_1.parent = this;
+	this.instance_1.setTransform(341.35,43.45,0.5,0.5);
+
+	this.instance_2 = new lib.CachedTexturedBitmap_11();
+	this.instance_2.parent = this;
+	this.instance_2.setTransform(401.7,437.9,0.5,0.5);
+
+	this.instance_3 = new lib.CachedTexturedBitmap_10();
+	this.instance_3.parent = this;
+	this.instance_3.setTransform(385.3,326.85,0.5,0.5);
+
+	this.instance_4 = new lib.CachedTexturedBitmap_9();
+	this.instance_4.parent = this;
+	this.instance_4.setTransform(343.4,160.25,0.5,0.5);
+
+	this.instance_5 = new lib.CachedTexturedBitmap_8();
+	this.instance_5.parent = this;
+	this.instance_5.setTransform(343.4,75.45,0.5,0.5);
+
+	this.instance_6 = new lib.CachedTexturedBitmap_12();
+	this.instance_6.parent = this;
+	this.instance_6.setTransform(325.35,59.45,0.5,0.5);
+
+	this.instance_7 = new lib.CachedTexturedBitmap_16();
+	this.instance_7.parent = this;
+	this.instance_7.setTransform(401.7,437.9,0.5,0.5);
+
+	this.instance_8 = new lib.CachedTexturedBitmap_15();
+	this.instance_8.parent = this;
+	this.instance_8.setTransform(385.3,326.85,0.5,0.5);
+
+	this.instance_9 = new lib.CachedTexturedBitmap_14();
+	this.instance_9.parent = this;
+	this.instance_9.setTransform(343.4,160.25,0.5,0.5);
+
+	this.instance_10 = new lib.CachedTexturedBitmap_13();
+	this.instance_10.parent = this;
+	this.instance_10.setTransform(343.4,75.45,0.5,0.5);
+
+	this.instance_11 = new lib.CachedTexturedBitmap_18();
+	this.instance_11.parent = this;
+	this.instance_11.setTransform(382.8,193.5,0.5,0.5);
+
+	this.instance_12 = new lib.CachedTexturedBitmap_19();
+	this.instance_12.parent = this;
+	this.instance_12.setTransform(323.35,51.45,0.5,0.5);
+
+	this.instance_13 = new lib.CachedTexturedBitmap_21();
+	this.instance_13.parent = this;
+	this.instance_13.setTransform(382.8,145.5,0.5,0.5);
+
+	this.instance_14 = new lib.CachedTexturedBitmap_20();
+	this.instance_14.parent = this;
+	this.instance_14.setTransform(356.8,77.5,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.instance}]}).to({state:[{t:this.instance_6},{t:this.instance_5},{t:this.instance_4},{t:this.instance_3},{t:this.instance_2}]},1).to({state:[{t:this.instance_6},{t:this.instance_10},{t:this.instance_9},{t:this.instance_8},{t:this.instance_7}]},1).to({state:[{t:this.instance_12},{t:this.instance_11}]},1).to({state:[{t:this.instance_12},{t:this.instance_14},{t:this.instance_13}]},1).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+
+
+(lib.场景_1_图层_2 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// 图层_2
+	this.instance = new lib.CachedTexturedBitmap_4();
+	this.instance.parent = this;
+	this.instance.setTransform(102.35,611.2,0.5,0.5);
+
+	this.instance_1 = new lib.CachedTexturedBitmap_3();
+	this.instance_1.parent = this;
+	this.instance_1.setTransform(103.45,403.95,0.5,0.5);
+
+	this.instance_2 = new lib.CachedTexturedBitmap_2();
+	this.instance_2.parent = this;
+	this.instance_2.setTransform(102.35,233.3,0.5,0.5);
+
+	this.instance_3 = new lib.CachedTexturedBitmap_1();
+	this.instance_3.parent = this;
+	this.instance_3.setTransform(102.35,47.35,0.5,0.5);
+
+	this.four = new lib.button4();
+	this.four.name = "four";
+	this.four.parent = this;
+	this.four.setTransform(37.4,609.15);
+	new cjs.ButtonHelper(this.four, 0, 1, 1);
+
+	this.two = new lib.button3();
+	this.two.name = "two";
+	this.two.parent = this;
+	this.two.setTransform(37.4,231.25);
+	new cjs.ButtonHelper(this.two, 0, 1, 1);
+
+	this.three = new lib.button2();
+	this.three.name = "three";
+	this.three.parent = this;
+	this.three.setTransform(37.4,407.75);
+	new cjs.ButtonHelper(this.three, 0, 1, 1);
+
+	this.one = new lib.button1();
+	this.one.name = "one";
+	this.one.parent = this;
+	this.one.setTransform(37.4,45.3);
+	new cjs.ButtonHelper(this.one, 0, 1, 1);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.one},{t:this.three},{t:this.two},{t:this.four},{t:this.instance_3},{t:this.instance_2},{t:this.instance_1},{t:this.instance}]}).wait(5));
+
+}).prototype = p = new cjs.MovieClip();
+
+
+// stage content:
+(lib.animate = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{n1:0,n2:1,n3:2,n4:3,n5:4});
+
+	this.___GetDepth___ = function(obj) {
+		var depth = obj.depth;
+		var cameraObj = this.___camera___instance;
+		if(cameraObj && cameraObj.depth && obj.isAttachedToCamera)
+		{
+			depth += depth + cameraObj.depth;
+		}
+		return depth;
+		}
+	this.___needSorting___ = function() {
+		for (var i = 0; i < this.getNumChildren() - 1; i++)
+		{
+			var prevDepth = this.___GetDepth___(this.getChildAt(i));
+			var nextDepth = this.___GetDepth___(this.getChildAt(i + 1));
+			if (prevDepth < nextDepth)
+				return true;
+		}
+		return false;
+	}
+	this.___sortFunction___ = function(obj1, obj2) {
+		return (this.exportRoot.___GetDepth___(obj2) - this.exportRoot.___GetDepth___(obj1));
+	}
+	this.on('tick', function (event){
+		var curTimeline = event.currentTarget;
+		if (curTimeline.___needSorting___()){
+			this.sortChildren(curTimeline.___sortFunction___);
+		}
+	});
+
+	// timeline functions:
+	this.frame_0 = function() {
+		this.one = this.图层_2.one;
+		this.three = this.图层_2.three;
+		this.two = this.图层_2.two;
+		this.four = this.图层_2.four;
+		var _this = this;
+		/*
+		停止播放影片剪辑/视频
+		停止播放指定影片剪辑或视频。
+		*/
+		_this.stop();
+		
+		
+		var _this = this;
+		/*
+		单击指定元件实例时将执行相应函数。
+		*/
+		_this.one.on('click', function(){
+		/*
+		将播放头移动到时间轴中的指定帧编号并停止播放影片。
+		可在主时间轴或影片剪辑时间轴上使用。
+		*/
+		_this.gotoAndStop('n2');
+		});
+		
+		
+		var _this = this;
+		/*
+		单击指定元件实例时将执行相应函数。
+		*/
+		_this.two.on('click', function(){
+		/*
+		将播放头移动到时间轴中的指定帧编号并停止播放影片。
+		可在主时间轴或影片剪辑时间轴上使用。
+		*/
+		_this.gotoAndStop('n3');
+		});
+		
+		var _this = this;
+		/*
+		单击指定元件实例时将执行相应函数。
+		*/
+		_this.three.on('click', function(){
+		/*
+		将播放头移动到时间轴中的指定帧编号并停止播放影片。
+		可在主时间轴或影片剪辑时间轴上使用。
+		*/
+		_this.gotoAndStop('n4');
+		});
+		
+		var _this = this;
+		/*
+		单击指定元件实例时将执行相应函数。
+		*/
+		_this.four.on('click', function(){
+		/*
+		将播放头移动到时间轴中的指定帧编号并停止播放影片。
+		可在主时间轴或影片剪辑时间轴上使用。
+		*/
+		_this.gotoAndStop('n5');
+		});
+	}
+	this.frame_2 = function() {
+		var _this = this;
+		/*
+		单击指定元件实例时将执行相应函数。
+		*/
+		_this.movieClip_7.on('click', function(){
+		/*
+		将播放头移动到时间轴中的指定帧编号并停止播放影片。
+		可在主时间轴或影片剪辑时间轴上使用。
+		*/
+		_this.gotoAndStop('n4');
+		});
+	}
+	this.frame_4 = function() {
+		this.___loopingOver___ = true;
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(2).call(this.frame_2).wait(2).call(this.frame_4).wait(1));
+
+	// 图层_2_obj_
+	this.图层_2 = new lib.场景_1_图层_2();
+	this.图层_2.name = "图层_2";
+	this.图层_2.parent = this;
+	this.图层_2.setTransform(134.1,367.9,1,1,0,0,0,134.1,367.9);
+	this.图层_2.depth = 0;
+	this.图层_2.isAttachedToCamera = 0
+	this.图层_2.isAttachedToMask = 0
+	this.图层_2.layerDepth = 0
+	this.图层_2.layerIndex = 0
+	this.图层_2.maskLayerName = 0
+
+	this.timeline.addTween(cjs.Tween.get(this.图层_2).wait(5));
+
+	// 图层_1_obj_
+	this.图层_1 = new lib.场景_1_图层_1();
+	this.图层_1.name = "图层_1";
+	this.图层_1.parent = this;
+	this.图层_1.setTransform(645.9,365.9,1,1,0,0,0,645.9,365.9);
+	this.图层_1.depth = 0;
+	this.图层_1.isAttachedToCamera = 0
+	this.图层_1.isAttachedToMask = 0
+	this.图层_1.layerDepth = 0
+	this.图层_1.layerIndex = 1
+	this.图层_1.maskLayerName = 0
+
+	this.timeline.addTween(cjs.Tween.get(this.图层_1).wait(5));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(542.9,422.8,447.5,274.3);
+// library properties:
+lib.properties = {
+	id: '0D6E580ACB8E014BB190CDA5753649EE',
+	width: 1024,
+	height: 768,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/animate_atlas_.png?1577687015138", id:"animate_atlas_"},
+		{src:"images/animate_atlas_2.png?1577687015138", id:"animate_atlas_2"},
+		{src:"images/animate_atlas_3.png?1577687015138", id:"animate_atlas_3"},
+		{src:"images/animate_atlas_4.png?1577687015139", id:"animate_atlas_4"},
+		{src:"images/animate_atlas_5.png?1577687015139", id:"animate_atlas_5"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['0D6E580ACB8E014BB190CDA5753649EE'] = {
+	getStage: function() { return exportRoot.getStage(); },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+// Layer depth API : 
+
+AdobeAn.Layer = new function() {
+	this.getLayerZDepth = function(timeline, layerName)
+	{
+		if(layerName === "Camera")
+		layerName = "___camera___instance";
+		var script = "if(timeline." + layerName + ") timeline." + layerName + ".depth; else 0;";
+		return eval(script);
+	}
+	this.setLayerZDepth = function(timeline, layerName, zDepth)
+	{
+		const MAX_zDepth = 10000;
+		const MIN_zDepth = -5000;
+		if(zDepth > MAX_zDepth)
+			zDepth = MAX_zDepth;
+		else if(zDepth < MIN_zDepth)
+			zDepth = MIN_zDepth;
+		if(layerName === "Camera")
+		layerName = "___camera___instance";
+		var script = "if(timeline." + layerName + ") timeline." + layerName + ".depth = " + zDepth + ";";
+		eval(script);
+	}
+	this.removeLayer = function(timeline, layerName)
+	{
+		if(layerName === "Camera")
+		layerName = "___camera___instance";
+		var script = "if(timeline." + layerName + ") timeline.removeChild(timeline." + layerName + ");";
+		eval(script);
+	}
+	this.addNewLayer = function(timeline, layerName, zDepth)
+	{
+		if(layerName === "Camera")
+		layerName = "___camera___instance";
+		zDepth = typeof zDepth !== 'undefined' ? zDepth : 0;
+		var layer = new createjs.MovieClip();
+		layer.name = layerName;
+		layer.depth = zDepth;
+		layer.layerIndex = 0;
+		timeline.addChild(layer);
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
